46bdaa60409fa538d1959a503f24a25b1fc9c5b4
Monse Vásquez <47286468+MonseV@users.noreply.github.com>
2022-02-24 17:55:16 -0600

    operadores aritméticos

---
+++ operaciones.py
@@ -0,0 +1,21 @@
+# Orden de Ejecucion:
+# -*- coding: utf-8 -*-
+# Negacion ¬
+# -*- coding: utf-8 -*-
+# Modulo %
+# Exponencial **
+# Multiplicacion y Division
+# Suma y Resta
+# Operadores Logicos
+
+print 3*2
+print 6/2
+print 6%2
+print 5/2
+print 5%2
+print float(5)/float(2)
+print 5.0/2.0
+print 3*2-2
+print 3*(2-2)
+print 2**3
+print 2**2
