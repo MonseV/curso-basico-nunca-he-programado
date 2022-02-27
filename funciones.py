410f7f24a5490cb92033e4c49b8405f392211f9f
Monse Vásquez <47286468+MonseV@users.noreply.github.com>
2022-02-27 12:32:46 -0600

    funciones

---
+++ funciones.py
@@ -0,0 +1,9 @@
+#funcion -> codigo reutilizable, se pueden llamar en cualquier parte
+
+#definir la funcion
+def di_hola(nombre):
+  print "Hola " + nombre
+
+#llamando a la funcion
+di_hola("Uriel")
+di_hola("Codigo Facilito")
